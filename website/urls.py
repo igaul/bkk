5adf067caf912cb1405deb4da41036cab180ff45
gaul@pdx.edu <gaul@pdx.edu>
2024-01-30 13:08:30 -0800

    fmt

--- website/urls.py
+++ website/urls.py
@@ -7,14 +7,12 @@ from website.views import index, splash
 # TODO: move this to project/urls.py
 
 
-oregon_urls = [
-    path("", index, name="index")
-]
+oregon_urls = [path("", index, name="index")]
 
 urlpatterns = [
-     path("", splash, name="splash"),
-     # for old links TODO: redirect map to new urls
-     path("", include(washingtonsite_old_urls)),
-     path("or/", include(oregon_urls)),
-        path("wa/", include(washingtonsite_urls)),
+    path("", splash, name="splash"),
+    # for old links TODO: redirect map to new urls
+    path("", include(washingtonsite_old_urls)),
+    path("or/", include(oregon_urls)),
+    path("wa/", include(washingtonsite_urls)),
 ]
