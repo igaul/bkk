16c3f68b637dfabb2199e7b89e42a7d6f4d7e138
gaul@pdx.edu <gaul@pdx.edu>
2024-01-30 12:09:56 -0800

    bk

--- washingtonsite/views.py
+++ washingtonsite/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+import json
+import logging
+from uuid import UUID, uuid4
 
-# Create your views here.
+from django.conf import settings
+from django.http import (HttpRequest, HttpResponseNotFound,
+                         HttpResponseRedirect, JsonResponse)
+from django.shortcuts import get_object_or_404
+from django.template.response import TemplateResponse
+from django.urls import reverse
+from django.views.decorators.http import require_http_methods
+
+# remap:
+# bkkwa.html -> index
+# bkklocationswa.html -> calendar
+# bkstorywa.html -> story
+# bkkmediawa.html -> media
+# bkklinkswa.html -> links
+# bkksongswa.html -> songs
+# bkkcontactwa.html -> contact
+
+def index(request: HttpRequest) -> TemplateResponse:
+    """Render the index page."""
+    return TemplateResponse(request, "washingtonsite/index.dj.html")
+
+def calendar(request: HttpRequest) -> TemplateResponse:
+    """Render the calender page."""
+    return TemplateResponse(request, "washingtonsite/calendar.dj.html")
+
+def story(request: HttpRequest) -> TemplateResponse:
+    """Render the story page."""
+    return TemplateResponse(request, "washingtonsite/story.dj.html")
+
+def media(request: HttpRequest) -> TemplateResponse:
+    """Render the media page."""
+    return TemplateResponse(request, "washingtonsite/media.dj.html")
+
+def links(request: HttpRequest) -> TemplateResponse:
+    """Render the links page."""
+    return TemplateResponse(request, "washingtonsite/links.dj.html")
+
+def songs(request: HttpRequest) -> TemplateResponse:
+    """Render the songs page."""
+    return TemplateResponse(request, "washingtonsite/songs.dj.html")
+
+def contact(request: HttpRequest) -> TemplateResponse:
+    """Render the contact page."""
+    return TemplateResponse(request, "washingtonsite/contact.dj.html")
