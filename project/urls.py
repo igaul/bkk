fbcd166b75c4115640ffa33e4af541161a050e56
gaul@pdx.edu <gaul@pdx.edu>
2024-01-30 10:53:49 -0800

    wip

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from website.urls import urlpatterns as website_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('hamsters/', admin.site.urls),
+    path("", include(website_urls))
 ]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+import json
+import logging
+from uuid import UUID, uuid4
 
-# Create your views here.
+from django.conf import settings
+from django.http import (HttpRequest, HttpResponseNotFound,
+                         HttpResponseRedirect, JsonResponse)
+from django.shortcuts import get_object_or_404
+from django.template.response import TemplateResponse
+from django.urls import reverse
+from django.views.decorators.http import require_http_methods
+
+
+def index(request: HttpRequest) -> TemplateResponse:
+    """Render the index page."""
+    return TemplateResponse(request, "website/index.html")
