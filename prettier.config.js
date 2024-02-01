3580c910528023fe5123b390e40b425cda9752ad
gaul@pdx.edu <gaul@pdx.edu>
2024-01-31 19:50:20 -0800

    wip

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
 /** @type {import("prettier").Config} */
 export default {
-  "plugins": ["prettier-plugin-tailwindcss", "prettier-plugin-astro"],
-  "trailingComma": "es5",
-  "arrowParens": "avoid"
-}
+  plugins: ["prettier-plugin-tailwindcss"],
+  trailingComma: "es5",
+  arrowParens: "avoid",
+};
