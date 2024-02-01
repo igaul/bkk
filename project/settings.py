e865c9d0b303d9c19dcfcec76ffe5e8f38c63d92
gaul@pdx.edu <gaul@pdx.edu>
2024-02-01 11:51:37 -0800

    bk

--- project/settings.py
+++ project/settings.py
@@ -66,6 +66,21 @@ if DEBUG:
 # Application definition
 
 INSTALLED_APPS = [
+    'wagtail.contrib.forms',
+'wagtail.contrib.redirects',
+'wagtail.embeds',
+'wagtail.sites',
+'wagtail.users',
+'wagtail.snippets',
+'wagtail.documents',
+'wagtail.images',
+'wagtail.search',
+'wagtail.admin',
+'wagtail',
+
+'modelcluster',
+'taggit',
+    #
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
